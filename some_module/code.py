931eb6bebdba70d6d45fa25155445977543fd300
Samuel Hinton <samuelreay@gmail.com>
2017-12-05 18:23:04 +1100

    Adding doco tag

--- some_module/code.py
+++ some_module/code.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def integrate_trapz(xs, ys):
+    """ Document me"""
     widths = np.diff(xs)
     midpoints = 0.5 * (ys[1:] + ys[:-1])
     area = np.sum(widths * midpoints)
