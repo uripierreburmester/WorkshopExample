364895bb978a7c28413f8eeced959087185cd528
Samuel Hinton <samuelreay@gmail.com>
2017-12-05 18:31:42 +1100

    Relative import for init

--- some_module/__init__.py
+++ some_module/__init__.py
@@ -3,7 +3,7 @@
 
  It is also good for removing unnecessary filenames. If you read the :code:`__init__.py` file, you can see that
  someone could now run :code:`from some_module import add_stuff` instead of
- having to run :code:`from some_module.file1 import add_stuff`.
+ having to run :code:`from some_module.code import integrate_trapz`.
 
  Also note that members without documentation (such as :code:`untested_subtract` do not appear in the doco).
 
