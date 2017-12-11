c1d4b02bd58806ea5ff42b8815777677f674c686
Samuel Hinton <samuelreay@gmail.com>
2017-12-11 12:01:33 +1000

    Adding another thing for people to do

--- some_module/code.py
+++ some_module/code.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 
+# def extract_sentence_containing_word(paragraph, word):
+#     """ Document and test me"""
+#     sentences = paragraph.split(".")
+#     selected = [s.strip() for s in sentences if word in s]
+#     return selected
+
+
 def integrate_trapz(xs, ys):
-    """ Document me"""
+    """ Document and test me"""
     widths = np.diff(xs)
     midpoints = 0.5 * (ys[1:] + ys[:-1])
     area = np.sum(widths * midpoints)
