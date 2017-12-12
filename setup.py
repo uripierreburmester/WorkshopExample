d71588c975e1ef220132ae15de6981062e267212
uripierreburmester <30432869+uripierreburmester@users.noreply.github.com>
2017-12-12 13:24:27 +1100

    Make changes
    Make changes

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
-      author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      url='http://github.com/uripierreburmester/WorkshopExample',
+      author='Uri Pierre Burmester',
+      author_email='pierredoestutoring@gmail.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
