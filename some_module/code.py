42263911519d01f7d5f0deb49fb7eccdac9bf80a
uripierreburmester <30432869+uripierreburmester@users.noreply.github.com>
2017-12-12 14:07:08 +1100

    Another test commit

--- some_module/code.py
+++ some_module/code.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 
-# def extract_sentence_containing_word(paragraph, word):
-#     """ Document and test me"""
-#     sentences = paragraph.split(".")
-#     selected = [s.strip() for s in sentences if word in s]
-#     return selected
+ def extract_sentence_containing_word(paragraph, word):
+     """ Document and test me"""
+     sentences = paragraph.split(".")
+     selected = [s.strip() for s in sentences if word in s]
+     return selected
 
+#test commit to test the .yml file and such
 
 def integrate_trapz(xs, ys):
     """ Document and test me"""
